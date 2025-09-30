c85fc3a4fab34326c1ad0c6b23a2d6857aee0007
Abdul Rahim <abdul.rahim@paysyslabs.com>
2025-09-30 12:43:18 +0500

    feat: caching the result after fetching from DB

--- src/schemas/schemas.module.ts
+++ src/schemas/schemas.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SchemasService } from './schemas.service';
 import { SchemasController } from './schemas.controller';
 import { KnexModule } from 'src/knex/knex.module';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
   controllers: [SchemasController],
   providers: [SchemasService],
-  imports: [KnexModule],
+  imports: [KnexModule, RedisModule],
 })
 export class SchemasModule {}
--- src/schemas/schemas.service.ts
+++ src/schemas/schemas.service.ts
@@ -2,16 +2,31 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateSchemaDto } from './dto/create-schema.dto';
 import { UpdateSchemaDto } from './dto/update-schema.dto';
 import { Knex } from 'knex';
+import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class SchemasService {
-  constructor(@Inject('KNEX_CONNECTION') private knex: Knex) {}
+  constructor(
+    @Inject('KNEX_CONNECTION') private knex: Knex,
+    private readonly redisService: RedisService,
+  ) {}
 
   async findByEndpointPath(endpointPath: string) {
+    const cacheKey = `schema:${endpointPath}`;
+    const cached = await this.redisService.get(cacheKey);
+    if (cached) {
+      return JSON.parse(cached);
+    }
+
+    //ofc not found in cache
     const result = await this.knex('schemastable')
       .select('schema')
       .where({ endpoint_path: endpointPath })
       .first();
+
+    if (result) {
+      await this.redisService.set(cacheKey, JSON.stringify(result), 3600);
+    }
 
     return result || null;
   }
